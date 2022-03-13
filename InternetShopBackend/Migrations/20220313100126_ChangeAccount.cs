7f1877f58cc2a75ae1ff14d1bc184440f9b029fc
Yuri <dr.dampel@yandex.ru>
2022-03-13 13:06:06 +0300

    dz16 dop

---
+++ InternetShopBackend/Migrations/20220313100126_ChangeAccount.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetShopBackend.Migrations
+{
+    public partial class ChangeAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Accounts");
+        }
+    }
+}
