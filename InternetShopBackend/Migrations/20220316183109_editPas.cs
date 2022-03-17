41e1420f79c9620f7a9985eed4df1923fdc2a671
Yuri <dr.dampel@yandex.ru>
2022-03-17 14:45:27 +0300

    DZ17 дополнительно

---
+++ InternetShopBackend/Migrations/20220316183109_editPas.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetShopBackend.Migrations
+{
+    public partial class editPas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Accounts",
+                newName: "HashePassword");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HashePassword",
+                table: "Accounts",
+                newName: "Password");
+        }
+    }
+}
