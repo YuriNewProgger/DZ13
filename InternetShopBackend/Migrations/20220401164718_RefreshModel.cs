6de2e1cf9bb9a97a18aff3a99ccd6b43b2ebe91a
Yuri <dr.dampel@yandex.ru>
2022-04-01 20:04:14 +0300

    Resolve conflict with bd

---
+++ InternetShopBackend/Migrations/20220401164718_RefreshModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetShopBackend.Migrations
+{
+    public partial class RefreshModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
