4ff93823f65a084aaaf5a1e18825a713867f1d97
Yuri <dr.dampel@yandex.ru>
2022-04-08 21:55:44 +0300

    Practice done V2

---
+++ InternetShopBackend/Migrations/20220408175426_addIsBan.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetShopBackend.Migrations
+{
+    public partial class addIsBan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Baskets_Products_ProductId",
+                table: "Baskets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Baskets_ProductId",
+                table: "Baskets");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdAcc",
+                table: "Baskets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBan",
+                table: "Accounts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdAcc",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "IsBan",
+                table: "Accounts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Baskets_ProductId",
+                table: "Baskets",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Baskets_Products_ProductId",
+                table: "Baskets",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
